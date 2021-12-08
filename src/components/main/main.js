77ce7486baecfec45af44e1433d9546ad2540283
trusteddev1325 <trusteddev1325@gmail.com>
2021-12-09 00:53:54 +0900

    update mobile

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -3,6 +3,42 @@ import React from "react";
 import "./main.css";
 
 class Main extends React.Component{
+    componentDidMount() {
+        window.addEventListener('scroll', this.isScrolledIntoView);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.isScrolledIntoView);
+    }
+    isScrolledIntoView() {
+        var element = document.querySelector('.graph-bar');
+        var position = element.getBoundingClientRect();
+
+        // checking whether fully visible
+        var graph1 = document.querySelector(".graph-bar.graph-bar-1")
+        var graph2 = document.querySelector(".graph-bar.graph-bar-2")
+        var graph3 = document.querySelector(".graph-bar.graph-bar-3")
+        var graph4 = document.querySelector(".graph-bar.graph-bar-4")
+        var graph5 = document.querySelector(".graph-bar.graph-bar-5")
+        var graph6 = document.querySelector(".graph-bar.graph-bar-6")
+        var graph7 = document.querySelector(".graph-bar.graph-bar-7")
+        var graph8 = document.querySelector(".graph-bar.graph-bar-8")
+        if (position.top >= 0 && position.bottom <= window.innerHeight) {
+            graph1.style.height="420px"
+            graph2.style.height="512px"
+            graph3.style.height="365px"
+            graph4.style.height="262px"
+            graph5.style.height="205px"
+            graph6.style.height="156px"
+            graph7.style.height="104px"
+            graph8.style.height="37px";
+            // console.log('Element is fully visible in screen');
+        }
+        // checking for partial visibility
+         if (position.top < window.innerHeight && position.bottom >= 0) {
+            // console.log('Element is partially visible in screen');
+         }
+    }
     render(){
         return(
             <section className="info info--graph info-white">
@@ -11,43 +47,44 @@ class Main extends React.Component{
                     <div className="graph-container">
                         <div className="graph-text">
                             <h3>How cool is my cat?</h3>
-                            <p>For starters - all cats are cool, remember? That said, Cool Cats are worth between 3 and 10 points. These points are determined by which items your Cool Cat is made up of. Common items like a beanie or a hat are worth fewer points than more rare items like a computer head or an ape outfit.</p><p>The points of your Cool Cat can contribute to certain contests and raffles, but will be even more useful for breeding in Gen 2!</p>
+                            <p>For starters - all cats are cool, remember? That said, Cool Cats are worth between 3 and 10 points. These points are determined by which items your Cool Cat is made up of. Common items like a beanie or a hat are worth fewer points than more rare items like a computer head or an ape outfit.</p>
+                            <p>The points of your Cool Cat can contribute to certain contests and raffles, but will be even more useful for breeding in Gen 2!</p>
                         </div>
                         <div className="graph show">
                             <div className="graph-section">
-                                <div className="graph-bar graph-bar-1" style={{height: "420px"}}>
+                                <div className="graph-bar graph-bar-1" >
                                     <span><b>3</b>pts</span><i>1,999 cats</i>
                                 </div>
-                                <div className="graph-bar graph-bar-2" style={{height: "512px"}}>
+                                <div className="graph-bar graph-bar-2" >
                                     <span><b>4</b>pts</span>
                                     <i>2,600 cats</i>
                                 </div>
                                 <label>COOL</label>
                             </div>
                             <div className="graph-section">
-                                <div className="graph-bar graph-bar-3" style={{height: "365px"}}>
+                                <div className="graph-bar graph-bar-3" >
                                     <span><b>5</b>pts</span>
                                     <i>1,750 cats</i>
                                 </div>
-                                <div className="graph-bar graph-bar-4" style={{height: "262px"}}>
+                                <div className="graph-bar graph-bar-4">
                                     <span><b>6</b>pts</span><i>1,250 cats</i>
                                 </div>
                                 <label>WILD</label>
                             </div>
                             <div className="graph-section">
-                                <div className="graph-bar graph-bar-5" style={{height: "205px"}}>
+                                <div className="graph-bar graph-bar-5">
                                     <span><b>7</b>pts</span><i>1,000 cats</i>
                                 </div>
-                                <div className="graph-bar graph-bar-6" style={{height: "156px"}}>
+                                <div className="graph-bar graph-bar-6">
                                     <span><b>8</b>pts</span><i>750 cats</i>
                                 </div>
                                 <label>CLASSY</label>
                             </div>
                             <div className="graph-section">
-                                <div className="graph-bar graph-bar-7" style={{height: "104px"}}>
+                                <div className="graph-bar graph-bar-7">
                                     <span><b>9</b>pts</span><i>500 cats</i>
                                 </div>
-                                <div className="graph-bar graph-bar-8" style={{height: "37px"}}>
+                                <div className="graph-bar graph-bar-8">
                                     <span><b>10</b>pts</span>
                                     <i>150 cats</i>
                                 </div>
